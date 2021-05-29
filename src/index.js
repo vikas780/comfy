9212c9ebf4ed8f6c15eb063151cc1855b26496eb
vikas780 <vikas1000dhiman@gmail.com>
2021-05-29 21:58:15 +0530

    Hide data

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 //{process.env.REACT_APP_CLIENT_ID}
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-cjw81cev.us.auth0.com"
-    clientId="XhfjNDredO3IiWa6pOjDv73jqMHETvec"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
